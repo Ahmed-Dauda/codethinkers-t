b04ed53627942b77541a3b50e2a65ca65f5cfbc2
Ahmed-Dauda <addatainsight@gmil.com>
2021-12-12 12:49:26 -0800

    First version of application moved into github

---
+++ users/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+
+# Register your models here.
+from users.models import NewUser
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('first_name','last_name','email', 'password','countries', 'phone_number','last_login')}),
+        ('Permissions', {'fields': (
+            'is_active', 
+            'is_staff', 
+            'is_superuser',
+            'groups', 
+            'user_permissions',
+        )}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('first_name','last_name','email','countries','phone_number' ,'password1', 'password2')
+            }
+        ),
+    )
+
+    list_display = ('first_name','last_name', 'email','countries', 'phone_number','is_staff', 'last_login')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
+
+admin.site.register(NewUser, UserAdmin)
+# admin.site.register(NewUser)
